97bd57e64f151aa8e408926c03aa48e2b6820ec4
Manuel Barral <manuba9017@gmail.com>
2021-09-22 01:23:29 -0300

    basic shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useSelector} from 'react-redux';
+
+import Cart from './Components/Cart/Cart';
+import Layout from './Components/Layout/Layout';
+import Products from './Components/Shop/Products';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+	return (
+		<Layout>
+			{showCart && <Cart />}
+			<Products />
+		</Layout>
+	);
 }
 
 export default App;
